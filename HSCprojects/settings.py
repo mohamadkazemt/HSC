245868310c4f70688df30153a493ae01bfdb3c12
mohamadkazemt <107670897+mohamadkazemt@users.noreply.github.com>
2024-10-22 23:39:50 +0330

    Update settings.py

--- HSCprojects/settings.py
+++ HSCprojects/settings.py
@@ -96,10 +96,15 @@ WSGI_APPLICATION = 'HSCprojects.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'HSCprojects_db',
+        'USER': 'HSCprojects_user',
+        'PASSWORD': 'Znmk@0900',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
