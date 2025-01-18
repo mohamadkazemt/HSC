e49a38e733ee98a151462df52eda8f68acab09f4
shayan <saleh@webshayan.com>
2025-01-18 19:29:05 +0330

    mkt-update3

--- hse_incidents/apps.py
+++ hse_incidents/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class HseIncidentsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'hse_incidents'
+    verbose_name = 'گزارش های حوادث'
--- machine_checklist/apps.py
+++ machine_checklist/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class MachineChecklistConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'machine_checklist'
+    verbose_name = 'چک لیست ماشین آلات'
