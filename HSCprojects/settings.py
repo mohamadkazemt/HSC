010a66544d7b95d71991d82a57920d8c9d019ad9
No1 <m.torkzade@gmail.com>
2025-02-28 09:19:42 +0330

    PWA

--- HSCprojects/settings.py
+++ HSCprojects/settings.py
@@ -18,7 +18,7 @@ from django.conf.locale.fa import formats as fa_formats
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
