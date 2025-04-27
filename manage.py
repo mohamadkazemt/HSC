b12b49d1a54cbe19975ab06e707cbdc5b0e61cdc
shayan <saleh@webshayan.com>
2025-04-27 18:02:46 +0330

    به‌روزرسانی تنظیمات محیط در manage.py و بهبود عملکرد فرم ثبت حادثه
    - افزودن تشخیص محیط اجرا در manage.py برای استفاده از تنظیمات مناسب.
    - به‌روزرسانی کد JavaScript در فرم ثبت حادثه برای بهبود عملکرد Select2 و مدیریت تغییرات فیلدها.
    - بهبود تعامل کاربر با افزودن پیام‌های مناسب و مدیریت بهتر چک‌باکس‌ها و فیلدهای زمان.

--- manage.py
+++ manage.py
@@ -6,7 +6,12 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HSCprojects.settings.production')
+    # تشخیص محیط اجرا
+    if os.environ.get('DJANGO_ENV') == 'production':
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HSCprojects.settings.production')
+    else:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HSCprojects.settings.development')
+    
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
