ee648c2ea5138aed24d0221d7097042a12f82124
mohamadkazemt <m.torkzade@gmail.com>
2025-10-28 23:06:05 +0330

    chore: remove all frontend templates and static assets

--- static/admin/main.js
+++
@@ -1,8 +0,0 @@
-/*
- * Persian Date Picker functionality has been removed from admin
- * Standard date inputs will be used instead
- */
-$(function () {
-    // Persian date picker functionality removed
-    console.info('Admin Persian date picker has been disabled');
-});
