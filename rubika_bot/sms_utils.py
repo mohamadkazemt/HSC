9922bc4d53a4a64ecb8dacc3660f882c0a2ad7c4
shayan <saleh@webshayan.com>
2025-11-14 09:22:19 +0330

    feat(sms): update SMS connection code handling and add detailed SMS template guide

--- rubika_bot/sms_utils.py
+++ rubika_bot/sms_utils.py
@@ -6,17 +6,33 @@ logger = logging.getLogger(__name__)
 
 sms_ir = SmsIr(api_key=settings.SMSIR_API_KEY, linenumber=settings.SMSIR_LINE_NUMBER)
 
-RUBIKA_CONNECTION_TEMPLATE = 999999
+RUBIKA_CONNECTION_TEMPLATE = 920200
 
 def send_connection_code_sms(mobile_number, connection_code):
     """
     ارسال کد اتصال روبیکا از طریق SMS
+    
+    قالب پیامک در پنل SMS.ir باید به این صورت باشد:
+    ---
+    کد اتصال روبیکا:
+    #CODE1##CODE2#
+    معتبر تا 60 دقیقه
+    ---
+    پارامترها: CODE1, CODE2
+    
+    توضیح: چون کد اتصال 32 کاراکتر است و محدودیت هر پارامتر 25 کاراکتر است،
+    کد را به دو قسمت تقسیم می‌کنیم.
     """
     try:
         logger.info(f"شروع ارسال کد اتصال به شماره {mobile_number}")
         
+        # تقسیم کد به دو قسمت به دلیل محدودیت 25 کاراکتری SMS.ir
+        code_part1 = connection_code[:25]  # 25 کاراکتر اول
+        code_part2 = connection_code[25:]  # باقیمانده (7 کاراکتر)
+        
         parameters = [
-            {"Name": "CODE", "Value": connection_code}
+            {"Name": "CODE1", "Value": code_part1},
+            {"Name": "CODE2", "Value": code_part2}
         ]
         
         response = sms_ir.send_verify_code(
